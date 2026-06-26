8c3d5c108c1e406ff4783b5d96ba0cfdf4732220
mateen-mahi <mateenmahihot@gmail.com>
2026-06-27 02:09:05 +0500

    Clean navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import { GuestRoute, ProtectedRoute, RoleRoute } from "./routes/RouteGuards";
-import Layout from "./components/Layout"
+import Layout from "./components/Layout";
 // Pages
 import Signup          from "./pages/Signup";
 import Signin          from "./pages/Signin";
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
-import "./Sidebar.css";
+import "../styles/Sidebar.css";
 
 export default function Layout() {
   return (
