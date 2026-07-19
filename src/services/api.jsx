4f084053193641fcf77a338fe17baf56ac24bc8a
mateen-mahi <mateenmahihot@gmail.com>
2026-07-19 08:07:04 +0500

    signout change

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://complete-auth-in-express-main.onrender.com/api/v1",
+  // baseURL: "https://complete-auth-in-express-main.onrender.com/api/v1",
+  baseURL:"http://localhost:8080//api/v1",
+
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
